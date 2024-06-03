38986febf142c246bf3880a9d918fce6ef954334
Gary.Butler <gary@tallemu.com>
2024-06-03 18:20:28 +1000

    Add tests with mocking

--- AddNumbers.Tests.ps1
+++ AddNumbers.Tests.ps1
@@ -3,14 +3,31 @@
 
 Describe "Add-Numbers" {
     It "should return the sum of two positive numbers" {
+        # Act & Assert
         Add-Numbers -a 2 -b 3 | Should Be 5
     }
 
     It "should return the sum of a positive and a negative number" {
+        # Act & Assert
         Add-Numbers -a 5 -b -3 | Should Be 2
     }
 
     It "should return the sum of two negative numbers" {
+        # Act & Assert
         Add-Numbers -a -2 -b -3 | Should Be -5
+    }
+}
+
+Describe "Add-WithRandomNumbers" {
+    It "should return the sum of the inputs and the mocked random number" {
+        # Arrange
+        Mock Get-RandomNumber { return 42 }
+
+        # Act
+        $result = Add-WithRandomNumbers -a 5 -b 10
+
+        #Assert
+        $result | Should Be 57 # 5 + 10 + 42
+        Assert-MockCalled Get-RandomNumber -Exactly 1
     }
 }
--- AddNumbers.ps1
+++ AddNumbers.ps1
@@ -1,3 +1,7 @@
+function Get-RandomNumber {
+    return Get-Random -Minimum 1 -Maximum 100
+}
+
 function Add-Numbers {
     param (
         [int]$a,
@@ -5,4 +9,14 @@ function Add-Numbers {
     )
 
     return $a + $b
+}
+
+function Add-WithRandomNumbers {
+    param (
+        [int]$a,
+        [int]$b
+    )
+
+    $randomNumber = Get-RandomNumber
+    return $a + $b + $randomNumber
 }
