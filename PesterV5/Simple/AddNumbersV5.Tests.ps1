8323f9fba19c9faa5ebaef586a1a493e37748502
Gary.Butler <gary.butler@myob.com>
2024-09-09 12:39:34 +1000

    Test the mocking of multiple calls to a function

--- PesterV5/Simple/AddNumbersV5.Tests.ps1
+++ PesterV5/Simple/AddNumbersV5.Tests.ps1
@@ -32,7 +32,7 @@ Describe "Add-Numbers" {
 
 Describe "Add-WithRandomNumbers" {
     Context "When mocking Get-RandomNumberV5 with hard coded values" {
-            It "should return the sum of the inputs and the mocked random number" {
+        It "should return the sum of the inputs and the mocked random number" {
             # Arrange
             Mock Get-RandomNumberV5 { return 42 }
 
@@ -57,6 +57,43 @@ Describe "Add-WithRandomNumbers" {
             #Assert
             $result | Should -Be 58 # 5 + 10 + 43
             Assert-MockCalled Get-RandomNumberV5 -Exactly 1
+        }
+    }
+
+    Context "When mocking Get-RandomNumberV5 with multiple variable values" {
+        AfterEach {
+            $script:mockedResult = $null
+        }
+
+        It "GIVEN_MocksWithVariableMultipleTimes_WHEN_Add-WithRandomNumbersV5_THEN_ShouldHaveDifferentResults" {
+            # Arrange
+            $script:mockedResult = 43
+
+            # Define a script block to handle the mock logic and retain state
+            $mockWithState = {
+                # Increment the call count each time the mock is called
+                $script:mockedResult++
+                return $script:mockedResult
+            }
+
+            Mock Get-RandomNumberV5 -MockWith $mockWithState
+
+            # Act
+            $result = Add-WithRandomNumbersV5 -a 5 -b 10
+            $result2 = Add-WithRandomNumbersV5 -a 5 -b 10
+
+
+            #Assert
+            $result | Should -Be 59 # 5 + 10 + 43 + 1
+            $result2 | Should -Be 60 # 5 + 10 + 43 + 1 + 1
+            Assert-MockCalled Get-RandomNumberV5 -Exactly 2
+        }
+    }
+
+    Context "When mocking ensure previous mocks were reset" {
+        It "GIVEN_PreviousScriptMockedVariable_THEN_PreviousMockShouldBeReset" {
+            #Assert
+            $script:mockedResult | Should -Be $null
         }
     }
 }
